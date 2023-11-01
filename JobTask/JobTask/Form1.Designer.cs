aa74a078a694e01139d47fecfdd7d33b6362912c
edvinbul <edvin.bulgak@mail.ru>
2023-11-01 18:04:29 +0300

    TaskWithThreads

---
+++ JobTask/JobTask/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿using System.Windows.Forms;
+
+namespace JobTask
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.ThreadID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.String = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15"});
+            this.comboBox1.Location = new System.Drawing.Point(59, 343);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(156, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(59, 281);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Calibri", 14.25F);
+            this.button2.Location = new System.Drawing.Point(140, 281);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 33);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ThreadID,
+            this.String});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(26, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(221, 263);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // ThreadID
+            // 
+            this.ThreadID.Text = "ThreadID";
+            this.ThreadID.Width = 110;
+            // 
+            // String
+            // 
+            this.String.Text = "String";
+            this.String.Width = 107;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 9F);
+            this.label1.Location = new System.Drawing.Point(56, 326);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 14);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Select number of threads";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 512);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "ThreadTask";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ColumnHeader ThreadID;
+        private System.Windows.Forms.ColumnHeader String;
+    }
+}
+
---
+++ JobTask/JobTask/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Data.SqlClient;
+
+namespace JobTask
+{
+    public partial class Form1 : Form
+    {
+        private volatile bool isRunning = true; // volatile guarantees that a variable will always be read and written directly from memory
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int selectedValue = ParseToInt();
+
+           
+            isRunning = true;
+
+            for (int i = 0; i < selectedValue; i++)
+            {
+                Thread thread = new Thread(PrintRandomWithDelay);
+                
+                thread.Start();
+                Thread.Sleep(100);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+          
+            isRunning = false;
+        }
+
+        // method of parsing from text to int
+        private int ParseToInt()
+        {
+            object selectedItem = comboBox1.SelectedItem;
+            if (selectedItem != null)
+            {
+                string selectedText = selectedItem.ToString();
+                if (int.TryParse(selectedText, out int selectedValue))
+                {
+                    return selectedValue;
+                }
+            }
+            return 0;
+        }
+
+        private void PrintRandomWithDelay()
+        {
+            while (isRunning)
+            {
+                //Generating random string length
+                int stringLength = new Random().Next(5, 11);
+                string randString = RandomString(stringLength);
+
+                //Generating random delay
+                Random random = new Random();
+                int delayMilliseconds = random.Next(500, 2000);
+                Thread.Sleep(delayMilliseconds);
+
+                string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=H:\\College\\C#\\JobTask\\JobTask\\Microsoft Access Database File.mdf;Integrated Security=True"; //database link
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand())
+                    {
+                        command.Connection = connection;
+                        command.CommandText = "INSERT INTO Access (ThreadID, Time, Data) VALUES (@ThreadID, @Time, @Data)";
+                        command.Parameters.AddWithValue("@ThreadID", Thread.CurrentThread.ManagedThreadId);
+                        command.Parameters.AddWithValue("@Time", DateTime.Now);
+                        command.Parameters.AddWithValue("@Data", randString);
+                        command.ExecuteNonQuery();
+                    }
+                }
+                
+            
+                //displaying in listView
+                int threadId = Thread.CurrentThread.ManagedThreadId;
+                ListViewItem item = new ListViewItem(threadId.ToString()); 
+                item.SubItems.Add(randString);
+
+               
+                listView1.Invoke((MethodInvoker)delegate {
+                    listView1.Items.Add(item);
+                });
+
+                if (listView1.Items.Count > 20)
+                {
+                    listView1.Invoke((MethodInvoker)delegate {
+                        listView1.Items.RemoveAt(0); 
+                    });
+                }
+            }
+        }
+
+        //random string generation method
+        static string RandomString(int length)
+        {
+            const string chars = "1234567890-=!@#$%^&*()_+[]\\{}|;':\",.<>?/abcdefghijklmnopqrstuvwxyz";
+         
+            
+            StringBuilder result = new StringBuilder();
+            Random random = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                result.Append(chars[random.Next(chars.Length)]);
+            }
+            return result.ToString();
+        }
+
+    }
+}
